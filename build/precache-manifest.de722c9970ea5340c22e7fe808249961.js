d4892be8266a39e37dca8e0627d1875ac4702841
Anjali060 <104251501+Anjali060@users.noreply.github.com>
2022-04-23 19:22:46 +0530

    Add files via upload

---
+++ build/precache-manifest.de722c9970ea5340c22e7fe808249961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d979dc7867d7dd83f399248761a942d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b0d31ee7d8e65eb31a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "875cd729e034c864cb8f",
+    "url": "/static/css/main.e141296e.chunk.css"
+  },
+  {
+    "revision": "f2b0d31ee7d8e65eb31a",
+    "url": "/static/js/2.f703ff4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f703ff4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875cd729e034c864cb8f",
+    "url": "/static/js/main.4aff40c8.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
